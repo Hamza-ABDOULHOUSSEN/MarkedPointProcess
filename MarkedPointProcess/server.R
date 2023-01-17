1cd42497300056f343c0f8dc91b2d92202a519d7
Hamza-ABDOULHOUSSEN <abd.hamza.ha@gmail.com>
2023-01-17 08:31:00 +0100

    Add get_data function

--- MarkedPointProcess/server.R
+++ MarkedPointProcess/server.R
@@ -4,18 +4,27 @@ library(markdown)
 
 shinyServer ( function (input , output ) {
   
+  get_data <- function(database_index) {
+    switch(
+      database_index,
+      "amacrine" = amacrine,
+      "anemones" = anemones,
+      "ants" = ants,
+      "betacells" = betacells,
+      "bramclecanes" = bramclecanes
+    )
+  }
+  
   output$data_about <- renderText({
     data = input$database_index
     includeMarkdown(paste("data/", data, ".md", sep=""))
   })
     
   output$plot_point_pattern <- renderPlot({
-      switch(
-        input$database_index,
-        "amacrine" = plot(amacrine, markscale=1),
-        "anemones" = plot(anemones, markscale=1)
-      )
-      
-    
+      database_index = input$database_index
+      data = get_data(database_index)
+      print(data)
+      plot(data, markscale=1, main=database_index)
   })
+  
 })
